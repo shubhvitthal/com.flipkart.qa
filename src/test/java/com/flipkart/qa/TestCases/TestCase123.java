1a4778be4f4e641f58a4953bdb5a407d26e47166
shubhvitthal <shubham.shubham168@gmail.com>
2022-09-23 22:43:07 +0530

    This is new test case

---
+++ src/test/java/com/flipkart/qa/TestCases/TestCase123.java
@@ -0,0 +1,6 @@
+package com.flipkart.qa.TestCases;
+
+public class TestCase123 {
+
+	//This is my new test case please review
+}
--- src/test/java/com/flipkart/qa/TestCases/TpTest.java
+++ src/test/java/com/flipkart/qa/TestCases/TpTest.java
@@ -2,11 +2,16 @@ package com.flipkart.qa.TestCases;
 
 import org.testng.annotations.Test;
 
+
+//This is tp for lead
+
+
 public class TpTest {
 
 	@Test
 	public void Test() {
 		System.out.println("Checking test");
+		
 	}
 	
 }
